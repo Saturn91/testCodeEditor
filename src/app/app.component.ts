7f6d4050e3534ebd50751c4fd78ce07f167edfa6
Manuel G <manuel.geissberger@gmail.com>
2021-10-07 15:54:37 +0200

    [ADD] Code editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CodeModel } from '@ngstack/code-editor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TestCodeEditor';
+
+  theme = 'vs-dark';
+
+  codeModel: CodeModel = {
+    language: 'lua',
+    uri: 'main.json',
+    value: `-- your code --
+function __init()
+  --your init code
+end
+
+function __update()
+  --your regulary called code
+end
+
+print('hello world')`,
+  };
+
+  options = {
+    contextmenu: true,
+    minimap: {
+      enabled: true,
+    },
+  };
+
+  onCodeChanged(value: string) {
+    console.log('CODE', value);
+  }
 }
